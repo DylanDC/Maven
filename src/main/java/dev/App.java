34ada9f488d736d2aa82766333977fe36408c2ad
Dylan <dupont.cail@gmail.com>
2018-07-16 16:03:04 +0200

    add *

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,19 @@
 package dev;
-
-/**
- * Hello world!
- *
- */
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
-
-public class App {
-  public static void main(String[] args) {
-    String asciiArt = FigletFont.convertOneLine("hello world !");
-    System.out.println(asciiArt);
-  }
-}
+/**
+* Hello world!
+*
+*/
+	public class App {
+		public static void main(String[] args) {
+			String titre = ResourceBundle.getBundle("application").getString("titre"); 
+			String asciiArt = FigletFont.convertOneLine(titre);
+			System.out.println(asciiArt);
+			
+			// affichage de l'environnement
+			String environnement = ResourceBundle.getBundle("application").getString(
+			"environnement");
+			System.out.println("Environnement : " + environnement);
+			}
+	}
